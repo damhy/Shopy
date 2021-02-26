9f61d85c7acd5ff54f167c37e10cbc74126d49da
damhy <77923432+damhy@users.noreply.github.com>
2021-02-26 23:09:49 +0100

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import Footer from './components/Footer'
+import Header from './components/Header'
+
+import { Container } from 'react-bootstrap'
+function App() {
+  return (
+    <>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>welcome to shopy</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
+  )
+}
+
+export default App
