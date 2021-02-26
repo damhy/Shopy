9a6cb80395ff4396d2194610adacea51260ff0c2
damhy <77923432+damhy@users.noreply.github.com>
2021-02-26 23:27:28 +0100

    header change color

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -4,7 +4,7 @@ import { Nav, Navbar, Container } from 'react-bootstrap'
 const Header = () => {
   return (
     <header>
-      <Navbar bg='light' variant='dark' expand='lg' collapseOnSelect>
+      <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
         <Container>
           <Navbar.Brand href='/'>Shopy</Navbar.Brand>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
