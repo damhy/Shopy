9179e56e20a044405fc01b62f750745f3b9485b4
damhy <77923432+damhy@users.noreply.github.com>
2021-02-27 17:14:39 +0100

    preparing the components and the pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,22 @@
 import Footer from './components/Footer'
 import Header from './components/Header'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import HomeScreen from '../src/screens/HomeScreen'
 
 import { Container } from 'react-bootstrap'
+import ProductScreen from './screens/ProductScreen'
 function App() {
   return (
-    <>
+    <Router>
       <Header />
       <main className='py-3'>
         <Container>
-          <h1>welcome to shopy</h1>
+          <Route path='/' component={HomeScreen} exact />
+          <Route path='/product/:id' component={ProductScreen} />
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   )
 }
 
